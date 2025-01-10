abc3199c3ec051bcef84b7fdff5cd7c2d063b1f8
Numzn <104069796+Numzn@users.noreply.github.com>
2025-01-10 06:23:28 -0800

    fix: update ESLint configuration for Vercel deployment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,40 @@
 module.exports = {
-  extends: ["react-app"],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
   rules: {
-    "react/prop-types": "off",
-    "no-unused-vars": "warn",
-    "react/no-unescaped-entities": ["error", {
-      "forbid": [
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'react/no-unescaped-entities': ['error', {
+      'forbid': [
         {
-          "char": ">",
-          "alternatives": ["&gt;"]
+          'char': '>',
+          'alternatives': ['&gt;']
         },
         {
-          "char": "}",
-          "alternatives": ["&#125;"]
+          'char': '}',
+          'alternatives': ['&#125;']
         }
       ]
     }]
